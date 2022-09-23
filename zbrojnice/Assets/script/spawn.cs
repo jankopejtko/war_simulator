dedf5ed19e1bbdfe0beed2be2ced95958649d09c
Jan Kopejtko <69723842+jankopejtko@users.noreply.github.com>
2022-09-23 15:49:23 +0200

    save 15:48 23.09.2022

--- zbrojnice/Assets/script/spawn.cs
+++ zbrojnice/Assets/script/spawn.cs
@@ -8,19 +8,36 @@ public class spawn : MonoBehaviour
     [SerializeField] private int rotation;
     [SerializeField] private GameObject soldier1;
     [SerializeField] private GameObject soldier2;
+    [SerializeField] private float min_x;
+    [SerializeField] private float max_x;
 
     void Start()
     {
-        for(int i = 0; i < numberOfSoldiers; i++) 
+        for (int i = 0; i < numberOfSoldiers; i++)
         {
             float n = Random.Range(0f, 10f);
-            if(n > 5f)
+            if (n > 5f)
             {
-                Instantiate(soldier1, new Vector3(Random.Range(0.5f, 7f), 8.45f, Random.Range(-4f,4f)), Quaternion.Euler(0,-90,0));
+                Instantiate(soldier1, new Vector3(Random.Range(min_x, max_x), 8.45f, Random.Range(-4f, 4f)), Quaternion.Euler(0, rotation, 0));
             }
-            else 
+            else
             {
-                Instantiate(soldier2, new Vector3(Random.Range(0.5f, 7f), 8.45f, Random.Range(-4f,4f)), Quaternion.Euler(0,-90,0));
+                Instantiate(soldier2, new Vector3(Random.Range(min_x, max_x), 8.45f, Random.Range(-4f, 4f)), Quaternion.Euler(0, rotation, 0));
+            }
+        }
+    }
+    public static void SpawnSoldiers(int number, GameObject soldier1, GameObject soldier2 = null, GameObject soldier3 = null, int rotation) 
+    {
+        for (int i = 0; i < number; i++)
+        {
+            float n = Random.Range(0f, 10f);
+            if (n > 5f)
+            {
+                Instantiate(soldier1, new Vector3(Random.Range(min_x, max_x), 8.45f, Random.Range(-4f, 4f)), Quaternion.Euler(0, rotation, 0));
+            }
+            else
+            {
+                Instantiate(soldier2, new Vector3(Random.Range(min_x, max_x), 8.45f, Random.Range(-4f, 4f)), Quaternion.Euler(0, rotation, 0));
             }
         }
     }
