71acb6fb58e804188d0d2a60116f469fe8134012
Jan Kopejtko <69723842+jankopejtko@users.noreply.github.com>
2022-09-23 14:18:59 +0200

    spawning adding arguments

--- zbrojnice/Assets/script/spawn.cs
+++ zbrojnice/Assets/script/spawn.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
-    [SerializeField] private GameObject soldier;
+    [SerializeField] private int numberOfSoldiers;
+    [SerializeField] private int rotation;
+    [SerializeField] private GameObject soldier1;
+    [SerializeField] private GameObject soldier2;
+
     void Start()
     {
-        Instantiate(soldier, new Vector3(, 8.5f, 0), Quaternion.identity);
+        for(int i = 0; i < numberOfSoldiers; i++) 
+        {
+            float n = Random.Range(0f, 10f);
+            if(n > 5f)
+            {
+                Instantiate(soldier1, new Vector3(Random.Range(0.5f, 7f), 8.45f, Random.Range(-4f,4f)), Quaternion.Euler(0,-90,0));
+            }
+            else 
+            {
+                Instantiate(soldier2, new Vector3(Random.Range(0.5f, 7f), 8.45f, Random.Range(-4f,4f)), Quaternion.Euler(0,-90,0));
+            }
+        }
     }
 }
